faabb16ce9a35c372da29ccfd4ac39388f2a5afd
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2015-08-09 13:06:07 +0530

    Nesting Question completed

--- OOP/0.2 Nesting.cpp
+++ OOP/0.2 Nesting.cpp
@@ -1,2 +1,39 @@
-/* For this example I am not getting an example
-for which we can use nested member functions */
+/*Write a suitable example, illustrate nesting of member functions,
+private member functions and array of objects in C++ programming*/
+#include <iostream>
+using namespace std;
+
+class Employee {
+  int ID;
+  string name;
+
+  void display();
+
+  public:
+  void input();
+}
+
+void Employee::input() {
+  cout<<"Enter name: ";
+  cin>>name;
+  cout<<"Enter Employee ID: ";
+  cin>>ID;
+
+  display();
+}
+
+void Employee::display() {
+  cout<<"Name : "<<name<<endl;
+  cout<<"ID: "<<ID<<endl;
+}
+
+int main() {
+  Employee E[10];
+  int i,n;
+  cout<<"How many Employees are there?: ";
+  cin>>n;
+
+  for (i = 0; i < n; i++) {
+    E[i].input();
+  }
+}
