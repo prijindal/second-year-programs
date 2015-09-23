cf440fb80bebfffcbd143e28514c45e5dfbbbf9e
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2015-09-23 10:45:19 +0530

    Create Q16 Varying Template.cpp

---
+++ OOP/Q16 Varying Template.cpp
@@ -0,0 +1,110 @@
+/*
+Write a cpp program to handle a generic queue which can handle different types of items like patient id
+has input or Price with decimals have input or Employee name has input to the queue.
+make use of class template
+*/
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+template <class T>
+class Queue {
+	T list[100];
+	int size;
+	public:
+		Queue() {
+			size = 0;
+		}
+		
+		void enqueue(T newData) {
+			list[size] = newData;
+			size++;
+		}
+		
+		T dequeue() {
+			int i;
+			T result = list[0];
+			for(i = 0;i<size-1;++i){
+				list[i] = list[i+1];
+			}
+			return result;
+		}
+		
+		void display() {
+			int i;
+			for(int i;i<size;++i) {
+				cout<<i+1<<": "<<list[i]<<endl;
+			}
+			cout<<'\n';
+		}
+		
+		void menu() {
+			int ch;
+			while(true) {
+				cout<<"Choose an operation\n";
+				cout<<"1. Enqueue\n";
+				cout<<"2. Dequeue\n";
+				cout<<"3. Display\n";
+				cout<<"4. Exit\n";
+				cin>>ch;
+				switch(ch) {
+					case 1:
+						T D;
+						cout<<"Enter new data\n";
+						cin>>D;
+						enqueue(D);
+						break;
+					case 2:
+						cout<<"Element Dequeud: "<<dequeue()<<endl;
+						break;
+					case 3:
+						display();
+						break;
+					case 4:
+						exit(1);
+					default:
+						cout<<"Invalid Operation\n";
+				}
+			}
+		}
+};
+
+void showMenu(int ch) {
+	switch(ch) {
+		case 1:
+		{
+			Queue<int> Q;
+			Q.menu();
+			break;
+		}
+		case 2:
+		{
+			Queue<float> Q;
+			Q.menu();
+			break;
+		}
+		case 3:
+		{
+			Queue<string> Q;
+			Q.menu();
+			break;
+		}
+		case 4:
+			exit(1);
+		default:
+			cout<<"Invalid option";
+	}
+}
+
+int main() {
+	int ch;
+	cout<<"Choose one data\n";
+	cout<<"1. Patient ID\n";
+	cout<<"2. Price\n";
+	cout<<"3. Employee name\n";
+	cout<<"4. None, Please take me out!!\n";
+	cin>>ch;
+	showMenu(ch);
+	
+}
